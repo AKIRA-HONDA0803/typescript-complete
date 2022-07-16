74ba072fe52966c7d487873282c338b529529718
KRofLife <05410kira@gmail.com>
2022-07-16 16:08:41 +0900

    12-16

---
+++ Section2/TypeScript-type/12-16index.ts
@@ -0,0 +1,25 @@
+let hasValue: boolean = true;
+let count: number = 10;
+let float: number = 3.14;
+let negative: number = -0.12;
+let single: string = 'hello';
+let double: string = "hello";
+let back: string = `hello`;
+let hello: string;
+hello = 'hello';
+
+const person = {
+  name: {
+    first: 'Jack',
+    last: 'Smith',
+  },
+  age: 21
+}
+
+// 型推論も可能
+// const person = {
+//   name: 'Jack',
+//   age: 21
+// }
+
+
