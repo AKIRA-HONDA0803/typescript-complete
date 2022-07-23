ae237365919249d51772580e950f937dafddd670
KRofLife <05410kira@gmail.com>
2022-07-23 13:01:10 +0900

    ゲッター・セッター

--- Section4/src/class.ts
+++ Section4/src/class.ts
@@ -12,7 +12,8 @@ class Person {
   // }
 
   // 簡略化
-  constructor(public readonly name:string, private age: number) {
+  constructor(public readonly name:string, protected age: number) {
+    // protectedは継承先でも使える
     // ここではreadonlyにならない
     this.id = 31;
     this.name = 'hehh'
@@ -38,3 +39,26 @@ let person2: Person;
 const quill = new Person('Quill', 38);
 quill.incrementAge();
 quill.greeting();
+
+class Teacher extends Person {
+  get subject() {
+    if (!this._subject) {
+    throw new Error('There is no subject');
+  }
+    return this._subject;
+  }
+  set subject(value) {
+    this._subject = value;
+  }
+  constructor(name: string, age: number, private _subject: string) {
+    super(name, age);
+  }
+  greeting() {
+    console.log(`Hello! My name is ${this.name}. I am ${this.age} years old`)
+  }
+}
+
+const teacher = new Teacher('Quill', 38, 'Math')
+teacher.subject = "Music"
+console.log(teacher.subject);
+teacher.greeting();
