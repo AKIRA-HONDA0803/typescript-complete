eb57807aab72b44007057aba5278de07fb88961d
KRofLife <05410kira@gmail.com>
2022-07-23 12:10:35 +0900

    public修飾子とprivate修飾子

--- Section4/src/class.ts
+++ Section4/src/class.ts
@@ -1,20 +1,32 @@
 class Person {
 
-  name: string;
+  // デフォルトはpublic
+  public name: string;
+  // privateを使うことでクラスの中でしか使えない
+  private age: number;
 
-  constructor(initName: string) {
+  constructor(initName: string, initAge:number) {
     this.name = initName;
+    this.age = initAge;
+  }
+  incrementAge() {
+    this.age += 1;
   }
 
   greeting(this: Person) {
-    console.log(`Hello! My name is ${this.name}`)
+    console.log(`Hello! My name is ${this.name}. I am ${this.age} years old`)
   }
 }
-const quill = new Person('Quill');
-quill.greeting()
-const anotherQuill = {
-  name: 'anotherQuill',
-  // thisを定義することで正常に動作させる
-  greeting: quill.greeting
-}
-anotherQuill.greeting
+// const quill = new Person('Quill');
+// quill.greeting()
+// const anotherQuill = {
+//   name: 'anotherQuill',
+//   // thisを定義することで正常に動作させる
+//   greeting: quill.greeting
+// }
+// anotherQuill.greeting
+
+let person2: Person;
+const quill = new Person('Quill', 38);
+quill.incrementAge();
+quill.greeting();
