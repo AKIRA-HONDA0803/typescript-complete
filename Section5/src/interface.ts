2b24fcac911c033fffe6fc9cac887a5b10b35735
KRofLife <05410kira@gmail.com>
2022-10-10 22:03:53 +0900

    implements・構造的部分型

--- Section5/src/interface.ts
+++ Section5/src/interface.ts
@@ -16,4 +16,24 @@ const human: Human = {
     console.log(message);
   }
 }
-let developer: Human;
+// let developer: Human;
+
+class Developer implements Human {
+  constructor(public name: string, public age: number, public experience: number)
+  greeting(message: string) {
+    console.log('Hello!');
+  }
+}
+const  tmpDeveloper = {
+  name: 'Quill',
+  age: 38,
+  experience: 3,
+  greeting(message: string) {
+    console.log('Hello!');
+  }
+}
+
+// Humanはexperienceがない => Developer側の要素が多くても大丈夫
+const user:Human = new Developer('Quill', 38, 3);
+// tmpDeveloperを一度変数にしないとexperienceでエラーが出る。
+// const user:Human = tmpDeveloper;
